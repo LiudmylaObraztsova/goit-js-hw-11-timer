79f135742a46f50ba32c6c6b2bae3bf909d519ba
LiudmylaObraztsova <lpfoneone@gmail.com>
2021-08-13 15:17:05 +0300

    add time formula

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
 import './sass/main.scss';
 
-const refs = {
-    days: document.querySelector('[data-value="days"]'),
-    hours: document.querySelector('[data-value="hours"]'),
-    minutes: document.querySelector('[data-value="minutes"]'),
-    seconds: document.querySelector('[data-value="seconds"]'),
+// const refs = {
+//     days: document.querySelector('[data-value="days"]'),
+//     hours: document.querySelector('[data-value="hours"]'),
+//     minutes: document.querySelector('[data-value="mins"]'),
+//     seconds: document.querySelector('[data-value="secs"]'),
+    
+// }
+// console.log('refs.days :>> ', refs.days); //проверка ссылки
+
+class countdownTimer {
+    constructor() {
+        this.refs = {
+            days: document.querySelector('[data-value="days"]'),
+            hours: document.querySelector('[data-value="hours"]'),
+            minutes: document.querySelector('[data-value="mins"]'),
+            seconds: document.querySelector('[data-value="secs"]'),
+            selector: document.querySelector(selector),
+        }
+        
+        this.targetDate = targetDate;
+        this.intervalId = null;
+            
+    }
+
+    getTimeComponents(time) {
+        const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+        const hours = this.pad(
+            Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+        );
+        const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    }
+
     
 }
-console.log('refs.days :>> ', refs.days);
