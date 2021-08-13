54e6f35f4098d36eb896804c9859cbac33cd5d24
LiudmylaObraztsova <lpfoneone@gmail.com>
2021-08-13 15:00:50 +0300

    make refs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './sass/main.scss';
+
+const refs = {
+    days: document.querySelector('[data-value="days"]'),
+    hours: document.querySelector('[data-value="hours"]'),
+    minutes: document.querySelector('[data-value="minutes"]'),
+    seconds: document.querySelector('[data-value="seconds"]'),
+    
+}
+console.log('refs.days :>> ', refs.days);
